99fd90f45c82ecf55510984ad20f355e3ad5bb8e
Virupaksha.VT <Virupaksha.VT@YCSLAP0256.mshome.net>
2021-02-14 23:30:52 +0530

    Added docker configuration and openapi swagger-ui implementation.

--- ppmTool/src/main/java/com/tool/PpmToolApplication.java
+++ ppmTool/src/main/java/com/tool/PpmToolApplication.java
@@ -5,9 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 
 @EnableJpaAuditing
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title="Explore PPMTOOL API",
+				description="API Definitions of the PPMTOOL Microservice",
+				version="1.0.1"
+		))
+
 public class PpmToolApplication {
 
 	public static void main(String[] args) {
--- ppmTool/src/main/java/com/tool/web/ProjectController.java
+++ ppmTool/src/main/java/com/tool/web/ProjectController.java
@@ -20,9 +20,15 @@ import com.tool.domain.Project;
 import com.tool.services.MapValidationErrorService;
 import com.tool.services.ProjectService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RequestMapping("/api/project")
 @RestController
 @CrossOrigin
+@Tag(name="Project",description="Project API")
 public class ProjectController {
 
 	@Autowired
@@ -31,6 +37,9 @@ public class ProjectController {
 	@Autowired
 	private MapValidationErrorService mapValidationErrorService;
 	
+	
+	@Operation(summary ="Create project")
+	@ApiResponses(value = {@ApiResponse(responseCode = "200",description = "Project successfully created.")})
 	@PostMapping("")
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project,BindingResult result){
 		
@@ -43,6 +52,9 @@ public class ProjectController {
 		return new ResponseEntity<Project>(project1,HttpStatus.CREATED);
 	}
 	
+	@Operation(summary ="Find project by id")
+	@ApiResponses(value = {@ApiResponse(responseCode = "200",description = "OK"),
+							@ApiResponse(responseCode = "400",description = "Project not found.")})
 	@GetMapping("/{projectId}")
 	public ResponseEntity<?> getProjectById(@PathVariable String projectId){
 		
@@ -52,12 +64,17 @@ public class ProjectController {
 		
 	}
 	
+	
+	@Operation(summary ="Find all projects")
+	@ApiResponses(value = {@ApiResponse(responseCode = "200",description = "OK")})
 	@GetMapping("/all")
 	public Iterable<Project> getAllProjects(){
 		
 		return projectService.findAllProjects();
 	}
 	
+	@Operation(summary ="Remove the project by id")
+	@ApiResponses(value = {@ApiResponse(responseCode = "200",description = "Successfully deleted.")})
 	@DeleteMapping("/{projectId}")
 	public ResponseEntity<?> deleteProject(@PathVariable String projectId){
 		
